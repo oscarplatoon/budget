c9a5d7e7dc4ec546ee35567d2680a48217dba48f
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-05 11:41:26 -0400

    Laid out psuedocode plot for implementing specified functionality

--- runner.py
+++ runner.py
@@ -2,3 +2,11 @@
 from classes.interface import BudgetInterface
 
 BudgetInterface("Test").run()
+
+# Design plan:
+# Runner just fires off the interface, which then creates an instance of Budget
+# Budget object will at instance time pull from a csv of expenses and income(s?)
+# Borrow the code from school_interface for .csv manipulation, accept the performance hit 
+# of rewriting the csv completely for each transaction
+# Budget class will be where budget/string formatted output methods happen for the relevant tests.
+# 
