9a154877a559d85386dbf7cf7aeee6268bf7f187
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-05 11:25:31 -0400

    First test working

--- budget_spec.py
+++ budget_spec.py
@@ -1,9 +1,12 @@
 import unittest
-from classes.interface import Interface
+from classes.interface import BudgetInterface
 
 class TestBudget(unittest.TestCase):
     """
     Does some test stuff
     """
     def test_budget_stuff(self):
-        self.assertEqual()
+        self.assertIsInstance(BudgetInterface('test').name, str)
+
+if __name__ == '__main__':
+    unittest.main()
--- classes/interface.py
+++ classes/interface.py
@@ -3,6 +3,6 @@ from .budget import Budget
 class BudgetInterface:
     def __init__(self, name) -> None:
         self.budget = Budget(name)
-
+        self.name = name
     def run(self):
         print(self.budget)
