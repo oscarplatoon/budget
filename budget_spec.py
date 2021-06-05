3ce4fbae607b87499d5ddc33fba8e861d2740350
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-05 11:37:44 -0400

    Built test structure for the rel 0 expected functionality

--- budget_spec.py
+++ budget_spec.py
@@ -3,10 +3,45 @@ from classes.interface import BudgetInterface
 
 class TestBudget(unittest.TestCase):
     """
-    Does some test stuff
+    Checks that BudgetInterface has a str name.
     """
-    def test_budget_stuff(self):
+    def test_basic_budget_functionality(self):
         self.assertIsInstance(BudgetInterface('test').name, str)
+
+    """
+    User can track expenses by category
+    """
+    def test_expense_by_category(self):
+    ###WIP
+        pass
+
+    """
+    User can adjust their monthly income.
+    """
+    def test_change_monthly_income(self):
+        pass
+
+    """
+    User can generate monthly costs.
+    """
+    #Fixed costs, utilities and stuff? Or also non fixed expenses? TBD
+    def test_generate_monthly_costs(self):
+        pass
+
+    """
+    User can create and calculate impacts of new expenses.
+    """
+    def test_create_new_expense(self):
+        pass
+
+    def test_create_new_expense_and_update_budget(self):
+        pass
+
+    """
+    The budget can calculate what percent of income is spent in a given category
+    """
+    def test_budget_percents(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
